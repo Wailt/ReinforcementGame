57a835fec8066219e85cd86bd8349a17d75f68cd
Wailt <vitaliy.miroshnychenko@magemojo.com>
2017-05-13 16:20:44 +0300

    update decision in brain

--- characters/brain.py
+++ characters/brain.py
@@ -1,5 +1,5 @@
 from characters.strategy import Strategy
-
+import numpy.random as npr
 
 METRICS = lambda one, two: ((one.x - two.x) + (one.y - two.y))  ** (0.5)
 
@@ -8,6 +8,8 @@ class Brain():
     def __init__(self, decisions):
         self.strategies = {'init': Strategy(loss=lambda x: x, decisions=decisions)}
 
-
     def decide(self, npc, world, strategy_name='init'):
-        self.strategies[strategy_name].decide(npc, world)
+        name, dec_list = self.strategies[strategy_name].decide(npc, world)
+        #ta ta ta
+        eval_loss = self.strategies[strategy_name].loss(npr.rand() * 2 - 1)
+        self.strategies[strategy_name].dec_weights[name] += eval_loss
--- characters/strategy.py
+++ characters/strategy.py
@@ -1,31 +1,41 @@
-import numpy.random as npr
 import numpy as np
+import numpy.random as npr
 
 temperature = 10
+
+
 class Strategy:
     def __init__(self, loss, decisions=[], n=3):
         self.decisions = decisions
         self.loss = loss
+
+        # {id: list}
         self.dec_weights = dict()
 
     def decide(self, npc, world):
         if npr.rand(len(self.dec_weights) + 1):
-            dec_list = self.create_new_decs()
+            name, dec_list = self.create_new_decs()
         else:
-            dec_list = self.choose_dec()
+            name, dec_list = self.choose_dec()
+
+        return name, dec_list
 
     def create_new_decs(self):
-        pass
+        dec_list = [self.decisions[i] for i in npr.randint(0, len(self.decisions))]
+        while dec_list in self.dec_weights:
+            dec_list = [self.decisions[i] for i in npr.randint(0, len(self.decisions))]
+
+        old_len = len(self.dec_weights)
+        self.dec_weights[old_len] = dec_list
+        return old_len, dec_list
 
     def choose_dec(self):
         norma = sum(np.exp(temperature * np.array([self.dec_weights[k] for k in self.dec_weights])))
         level = npr.rand()
         cumsum = 0
         for key in self.dec_weights:
-            # TODO: here temperature
             cumsum += np.exp(temperature * self.dec_weights[key]) / norma
             if cumsum >= level:
-                pass
-#                return {'moove': (key, decision['moove'][key])}
-#        return {'moove': ('random', decision['moove'][''])}
+                return key, self.dec_weights[key]
 
+# return {'moove': ('random', decision['moove'][''])}
