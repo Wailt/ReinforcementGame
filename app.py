719201911a9bd3738fd382d5f70d22c835f6414c
Wailt <vitaliy.miroshnychenko@magemojo.com>
2017-05-13 11:41:21 +0300

    Add main module app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame import *
+
+from time import time
+
+display = (400, 400)
+bg_color = "#FFFFFF"
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(display)
+    pygame.display.set_caption("Rein Game")
+    bg = Surface(display)
+    bg.fill(Color(bg_color))
+
+    timer = pygame.time.Clock()
+
+    step = 0
+    begin_time = time()
+    timer.tick(100)
+    try:
+        while 1:
+                pygame.display.update()
+                print('step:', step/(time() - begin_time))
+    except Exception as e:
+        print('step:', step)
+        print('time:', time() - begin_time)
+        raise e
+
+
+if __name__ == "__main__":
+    main()
