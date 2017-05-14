3e32f07556b16acf525a36ea85925530b0cce1a9
Wailt <vitaliy.miroshnychenko@magemojo.com>
2017-05-14 08:43:55 +0300

    срфтпу ыещту зкщиф

--- environment/environment.py
+++ environment/environment.py
@@ -1,19 +1,13 @@
 from environment.cell import Cell
 import numpy.random as npr
-from pygame import *
-
 
 class Environment:
     def __init__(self, width, height):
-        self.env_width = width
-        self.env_height = height
-        #self.map = [[Cell(i, j, img=('img/grass_' + str(npr.randint(1, 4)) + '.png') if npr.randint(10) else 'img/stone.png')
-        #             for j in range(height)] for i in range(width)]
         self.map = []
         for i in range(width):
             f = []
             for j in range(height):
-                if npr.randint(10):
+                if npr.randint(19):
                     f.append(Cell(i, j, img='img/grass_' + str(npr.randint(1, 4)) + '.png'))
                 else:
                     cell = Cell(i, j, img='img/stone.png')
